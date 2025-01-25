3fcdfb8b3241f98ac28d8ba158c658e69ae06c9d
emertechie <emertechie@gmail.com>
2025-01-25 17:56:30 +0000

    Add test to repro binding bug & fix

--- e2e/components.spec.ts
+++ e2e/components.spec.ts
@@ -40,3 +40,32 @@ test("can render checkbox page", async ({ page }) => {
   const mustBeChecked = questionPage.getByLabel("Must be checked");
   await expect(mustBeChecked).toBeChecked();
 });
+
+test("can switch answer type", async ({ page }) => {
+  await page.goto("http://localhost:5173/test");
+
+  const questionPage = page.getByTestId("features-question");
+
+  // Note: there was a bug where placeholder was not reset on changing answer type, so check here and later on
+  const placeholder = questionPage.getByLabel("Placeholder");
+  await expect(placeholder).toHaveValue("Enter your answer");
+
+  // Change answer type to checkbox
+  await questionPage.getByRole("combobox", { name: "Answer Type" }).click();
+  await page.getByLabel("Checkbox", { exact: true }).click();
+  await expect(questionPage.getByRole("combobox", { name: "Answer Type" })).toHaveText("Checkbox");
+
+  // Ensure default values rendered for checkbox answer
+  await expect(questionPage.getByLabel("Label")).toHaveValue("");
+  await expect(questionPage.getByLabel("Must be checked")).not.toBeChecked();
+
+  // Change answer type back to text
+  await questionPage.getByRole("combobox", { name: "Answer Type" }).click();
+  await page.getByLabel("Text", { exact: true }).click();
+  await expect(questionPage.getByRole("combobox", { name: "Answer Type" })).toHaveText("Text");
+
+  // Ensure default values rendered for text answer
+  await expect(questionPage.getByLabel("Multiline")).not.toBeChecked();
+  await expect(questionPage.getByLabel("Placeholder")).toHaveValue(""); // NOTE: bug fix
+  await expect(questionPage.getByLabel("Required")).not.toBeChecked();
+});
