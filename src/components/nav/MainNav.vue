819c59fff4ba2380aab01895a5b79b72366411df
emertechie <emertechie@gmail.com>
2025-02-07 19:38:05 +0000

    Show not implemented tooltip for nav items

--- src/components/nav/MainNav.vue
+++ src/components/nav/MainNav.vue
@@ -1,18 +1,47 @@
-<script setup lang="ts">
-import { cn } from "@/lib/utils";
-</script>
-
 <template>
   <nav :class="cn('flex items-center space-x-4 lg:space-x-6', $attrs.class ?? '')">
-    <a class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-      Overview
-    </a>
-    <a class="text-sm font-medium transition-colors hover:text-primary"> Surveys </a>
-    <a class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-      Templates
-    </a>
-    <a class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-      Settings
-    </a>
+    <TooltipProvider
+      v-for="item in navItems"
+      :key="item.label"
+    >
+      <Tooltip
+        :delay-duration="0"
+        :disabled="item.implemented"
+      >
+        <TooltipTrigger as-child>
+          <a
+            class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+            :class="{ 'text-primary': item.implemented }"
+          >
+            {{ item.label }}
+          </a>
+        </TooltipTrigger>
+        <TooltipContent> Not Implemented </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   </nav>
 </template>
+
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+const navItems = [
+  {
+    label: "Overview",
+    implemented: false,
+  },
+  {
+    label: "Surveys",
+    implemented: true,
+  },
+  {
+    label: "Templates",
+    implemented: false,
+  },
+  {
+    label: "Settings",
+    implemented: false,
+  },
+];
+</script>
