a9eebceec9edace188a762d4cb4dd46907d59c66
emertechie <emertechie@gmail.com>
2025-02-08 16:42:01 +0000

    Add WIP label

--- src/components/nav/TopNav.vue
+++ src/components/nav/TopNav.vue
@@ -1,7 +1,10 @@
 <template>
-  <nav class="flex h-16 items-center border-b px-3">
-    <h1 class="mr-16 text-xl font-bold">Swift Survey</h1>
-    <MainNav class="mr-auto" />
+  <nav class="flex h-16 items-center justify-between border-b px-3">
+    <div class="flex items-center gap-2">
+      <h1 class="text-xl font-bold">Swift Survey</h1>
+      <div class="ml-1 rounded-lg bg-amber-600 px-3 py-1 text-[10px] text-white">Early WIP!</div>
+      <MainNav class="ml-10 hidden sm:block" />
+    </div>
     <UserNav />
   </nav>
 </template>
